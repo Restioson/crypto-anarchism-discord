4378e43b596ddb6a18d64aa7e167b10961612330
Restioson <restiosondev@gmail.com>
2019-05-20 16:23:54 +0200

    Update to latest serenity (0.6.0rc1)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use serenity::command;
 use serenity::client::Client;
-use serenity::prelude::EventHandler;
-use serenity::framework::standard::StandardFramework;
+use serenity::prelude::*;
+use serenity::model::prelude::*;
+use serenity::framework::standard::macros::{group, command};
+use serenity::framework::standard::{CommandResult, StandardFramework, Args};
 use std::env;
 
 macro_rules! catch {
@@ -19,9 +20,10 @@ fn main() {
         .expect("Error creating client");
 
     client.with_framework(StandardFramework::new()
-        .configure(|c| c.prefix("a!"))
-        .cmd("ping", ping)
-        .cmd("vote", vote));
+        .configure(|c|
+            c.prefix("a!"))
+            .group(&COMMANDS_GROUP)
+    );
 
     println!("Starting bot...");
 
@@ -29,22 +31,39 @@ fn main() {
         eprintln!("Error while running client: {:?}", e);
     }
 }
-
-command!(ping(_context, message) {
-    if let Err(e) = message.reply("Pong!") {
+#[command]
+#[description = "Start a vote"]
+#[usage("#vote <vote text>")]
+fn ping(context: &mut Context, message: &Message, _: Args) -> CommandResult {
+    if let Err(e) = message.reply(&context, "Pong!") {
         eprintln!("Error returning ping: {:?}", e);
     }
-});
 
-command!(vote(_context, message) {
+    Ok(())
+}
+
+#[command]
+#[description = "Start a vote"]
+#[usage("#vote <vote text>")]
+fn vote(context: &mut Context, message: &Message, _: Args) -> CommandResult {
     let res = catch! {
         let vote_content = message.content.split(' ').skip(1).collect::<Vec<&str>>().join(" ");
-        let message = message.channel_id.say(format!("**Vote:** {}", vote_content))?;
-        message.react( '👍')?;
-        message.react('👎')
+        let old_message = message;
+        let message = message.channel_id.say(&context, format!("**Vote:** {}", vote_content))?;
+        message.react(&context, '👍')?;
+        message.react(&context, '👎')?;
+        old_message.delete(&context)
     };
 
     if let Err(e) = res {
         eprintln!("Error starting vote: {:?}", e);
     }
+
+    Ok(())
+}
+
+group!({
+  name: "Commands",
+  options: {},
+  commands: [ping, vote],
 });
